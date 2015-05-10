ed0fcb5fb229979761b23103f73c803a02cb1b84
suvankar <suvankar.17@gmail.com>
2015-05-10 18:26:28 +0530

    Adding input validation

--- 100/maxCycleLength.cpp
+++ 100/maxCycleLength.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <stdlib.h> 
 
 using std::string;
+
+const int upper_limit = 10000;
+const int lower_limit = 0;
 
 int cycle_length(int n){
   if (n == 1) {
@@ -25,11 +29,23 @@ int max_cycle_length(int begin, int end) {
   return max;
 }
 
+void usages(){
+	std::cout << "ERROR: Invalid Inputs" << std::endl;
+	std::cout << "Input must be in between " << upper_limit << " and " << lower_limit << std::endl; 
+		
+	exit (EXIT_FAILURE);
+}
+
 int main() {
 	int begining;
 	int end;
+	
+
 	std::cout << "Please enter input pair, seperated by space: ";
 	std::cin >> begining >> end ;
+	if (begining - lower_limit <= upper_limit - lower_limit){
+		usages();
+	}
 
 	std::cout << begining << " " << end << " " <<max_cycle_length(begining, end) << std::endl;
 	return 0;
