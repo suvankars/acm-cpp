b15363c09152b747aa5b3a5343110ba751533173
suvankar <suvankar.17@gmail.com>
2015-05-12 21:57:35 +0530

    Implemented 941 - nth_permutation

--- 941/permutation.cpp
+++ 941/permutation.cpp
@@ -9,31 +9,36 @@ using std::getline;
  
 string nth_permutation(std::string str, int n){
     
-   for(int i=0; i<3; i++){
+   for(int i=0; i<n; i++){
     std::next_permutation(str.begin(), str.end());
      
   }
     return str;
 }
 
+int print_permutation(int count){
+  for (int n=0; n < count ; n ++){
+    string str;
+    string per_count;
+    for (int n=0; n < 2 ; n ++){
+      
+      if(n%2==0){
+        std::getline(std::cin, str);
+      }
+      else{
+          std::getline(std::cin, per_count);
+      }
+
+    }
+    std::cout << nth_permutation(str, std::atoi( per_count.c_str() )) << std::endl;  
+  }
+}
+
 int main() {
   string line_count;
   std::getline(std::cin, line_count);
   int count = std::atoi( line_count.c_str() );
-  
-  //
-  // for (int n=0; n < count ; n ++){
-    
-  //   if(n%2==0){
-  //     std::getline(std::cin, str);
-  //     std::cout << str;
-  //   }
-  //   else{
-  //      //get permutation count
-  //   }
-  // }
-  // std::cout << line_count;
-  std::cout << nth_permutation("abc", 2) << std::endl;
+  print_permutation(count);
   return 0;
 }
 
