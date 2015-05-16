09d47987c711b7d18b85d9c1407ec744c7791521
suvankar <suvankar.17@gmail.com>
2015-05-17 01:08:43 +0530

    Changed data structure

--- 673/parentheses_balance.cpp
+++ 673/parentheses_balance.cpp
@@ -3,78 +3,65 @@
 #include <vector>
 #include <map>
 
+#include <stack>
+
 using namespace std;
 
-// To execute C++, please define "int main()"
+std::string isBalanced(std::string parentheses_str){
+  vector<char> parentheses(parentheses_str.begin(), parentheses_str.end());
+  std::stack<char> matcher;
+  std::map<char, char> brace_pair;
+  brace_pair['('] = ')';
+  brace_pair['['] = ']';
 
-// std::vector<std::string> &split(std::string str, char delim, std::vector<std::string> &elements){
-//   std::stringstream ss(str);
-//   std::string item;
+  char opening_brace1 = '(';
+  char opening_brace2 = '[';
 
-//   while(std::getline(ss, item, delim)){
-//     elements.push_back(item);
-//   }
-//   return elements;
-// }
-
-// std::vector<std::string> split(std::string str, char delim){
-//   std::vector<std::string> elements ;
-//   split(str, delim, elements);
-//   return elements;
-// }
+  char closing_brace1 = ')';
+  char closing_brace2 = ']';
 
 
-std::string isBalanced(std::string parentheses){
-  vector<char> elements(parentheses.begin(), parentheses.end());
-  std::map <char, char> matches;
-  matches['('] = ')';
-  matches['['] = ']';
-
-
-  std::string balanced = "Yes";
-  for(std::map<char, char>::iterator it = matches.begin(); it != matches.end(); it++) {
-    char opening_brace = it->first;
-    char closing_brace = it->second ;
-
-
-    int l = elements.size();
-    std::string found = "Yes";
-   // std::string individual_match = "No";
-    for(std::vector<char>::size_type i = 0; i != elements.size() ; i++) {
-
-        if(elements[i] == opening_brace){
-         std::cout << " going to found match for" << elements[i] <<std::endl;
-          std::string i_match = "No";
-          for(std::vector<char>::size_type j = l-1; j != i ; j--){
-            if(elements[j] == closing_brace){
-               i_match = "Yes";
-               std::cout << " found match for " << elements[i] << " with " << elements[j] <<std::endl;
-               l = j;
-               break;
-            }
-           }
-
-          if(  i_match == "Yes"){
-            found = "Yes";
-          }else{
-            found = "No";
-            std::cout << " NOT found match for " << i_match  <<std::endl;
+  for(std::vector<char>::size_type i = 0; i != parentheses.size(); i++  ){
+    if ((parentheses[i] == opening_brace1) || (parentheses[i] == opening_brace2) ){
+      std::cout << " going to push " << parentheses[i] << std::endl;
+      matcher.push(parentheses[i]);
+    }
+    else if ((parentheses[i] == closing_brace1) || (parentheses[i] == closing_brace2)){
+       std::cout << " going to pop " << parentheses[i] << std::endl;
+      char top_elm;
+      if(!matcher.empty()){
+        top_elm = matcher.top();
+      }
+        if(brace_pair[top_elm] == parentheses[i]){
+          std::cout << " going to remove " << parentheses[i] << std::endl;
+          if(!matcher.empty()){
+            matcher.pop();
           }
-         }
+        else{
+          std::cout << "push remaining elements " << parentheses[i] << std::endl;
+          matcher.push(parentheses[i]);
+        }
+
       }
 
 
-    if(found=="No"){
-      balanced = "No";
-      break;
     }
+
+  }
+
+  std::string balanced = "No";
+
+  if (matcher.empty()) {
+   balanced = "Yes";
   }
 
   return balanced;
 }
 
 int main() {
-  std::string parentheses = "(([]))";
-  std::cout << isBalanced(parentheses) << std::endl;
+  std::string parentheses_str = "(([()])))";
+  std::cout << isBalanced(parentheses_str) << std::endl;
+  std::string parentheses_str2 = "([()[]()])()";
+  std::cout << isBalanced(parentheses_str2) << std::endl;
   return 0;
 }
